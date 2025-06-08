8cbae4bef590cb17b95521bafc4882b1e4471a98
QHuyyyyy <huy120904@gmail.com>
2025-06-08 15:18:14 +0700

    CRUD for bloodrequest and user

--- backend/src/services/bloodrequest.service.ts
+++ backend/src/services/bloodrequest.service.ts
@@ -1,20 +1,45 @@
-import { BloodRequest } from "../models";
+import { BloodRequest, User } from "../models";
 import { BloodRequestInput, BloodRequestQuery } from "../types/bloodrequest.type";
 
-export class BloodRequestService {
-    async createBloodRequest(data: BloodRequestInput) {
-        const bloodRequest = new BloodRequest(data);
-        return await bloodRequest.save();
+
+export async function createBloodRequest(data: BloodRequestInput) {
+    const user = await User.findById(data.userId);
+    if (!user) {
+        throw new Error("User not found");
     }
+    const bloodRequest = new BloodRequest(data);
+    return await bloodRequest.save();
+}
 
-    async getBloodRequests(query: BloodRequestQuery) {
-        const filter: BloodRequestQuery = {};
+export async function getBloodRequests(query: BloodRequestQuery) {
+    const filter: BloodRequestQuery = {};
 
-        if (query.userId) filter.userId = query.userId;
-        if (query.bloodType) filter.bloodType = query.bloodType;
-        if (query.bloodComponent) filter.bloodComponent = query.bloodComponent;
-        if (query.status) filter.status = query.status;
+    if (query.userId) filter.userId = query.userId;
+    if (query.bloodType) filter.bloodType = query.bloodType;
+    if (query.bloodComponent) filter.bloodComponent = query.bloodComponent;
+    if (query.status) filter.status = query.status;
 
-        return BloodRequest.find(filter).populate("userId");
+    return BloodRequest.find(filter).populate("userId");
+}
+
+export async function findBloodRequestById(id: string) {
+    const bloodRequest = await BloodRequest.findById(id).populate("userId");
+    if (!bloodRequest) {
+        throw new Error("Blood request not found");
     }
+    return bloodRequest;
+}
+export async function updateBloodRequest(id: string, data: Partial<BloodRequestInput>) {
+    const bloodRequest = await BloodRequest.findByIdAndUpdate(id, data, { new: true }).populate("userId");
+    if (!bloodRequest) {
+        throw new Error("Blood request not found");
+    }
+    return bloodRequest;
+}
+export async function deleteBloodRequest(id: string) {
+    const bloodRequest = await BloodRequest.findByIdAndDelete(id);
+    if (!bloodRequest) {
+        throw new Error("Blood request not found");
+    }
+    return bloodRequest;
 }
